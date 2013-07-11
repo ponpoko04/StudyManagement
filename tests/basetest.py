a10cb0137d693088f153d546ac9e3fe095782ba5
Takayoshi Uchida <taum0715@gmail.com>
2013-07-12 00:05:44 +0900

    Datastoreローカルテスト完了まで

--- tests/basetest.py
+++ tests/basetest.py
@@ -16,10 +16,13 @@ from google.appengine.api import apiproxy_stub_map
 from google.appengine.api import datastore_file_stub
 from google.appengine.api import user_service_stub
 from google.appengine.api import users
+from google.appengine.tools import dev_appserver_login
 
 #テスト対象のクラスをインポート
 #from models import *
 from mainControler import mainControler
+import dsmodels
+
 
 APP_ID = u'test_id'
 AUTH_DOMAIN = 'gmail.com'
@@ -49,10 +52,57 @@ class GAETestBase(unittest.TestCase):
 #mainControlerクラステスト
 class test_mainControler(GAETestBase):
 
-    def test_createLoginInfo(self):
+    def test_getYearsList(self):
+        #年の配列を取得するテスト
+        #リストの３番目に現在年が格納されていることを確認
         controler = mainControler()
-        controler.createLoginInfo(users.get_current_user())
-        self.assertTrue(controler.isAuth, 'test')
+        years = controler.getYearsList()
+        self.assertEqual(controler.d.year, years[02].value, 'not now year')
+
+    def test_getMonthsList(self):
+        #月の配列を取得するテスト
+        #リストの選択フラグ=Trueを持つオブジェクトに現在月が格納されていることを確認
+        controler = mainControler()
+        months = controler.getMonthsList()
+        cftarget = str(controler.d.month).zfill(2)
+        for month in months:
+            if month.selected:
+                self.assertEqual(cftarget, month.value, 'not now month')
+
+    def test_getDaysList(self):
+        #日の配列を取得するテスト
+        #リストの選択フラグ=Tureを持つオブジェクトに現在月が格納されていることを確認
+        controler = mainControler()
+        days = controler.getDaysList()
+        cftarget = str(controler.d.day).zfill(2)
+        for day in days:
+            if day.selected:
+                self.assertEqual(cftarget, day.value, 'not now day')
+
+    def test_sumUpStudyHours_registercount(self):
+        #科目別勉強時間集計処理のテスト
+        #勉強単位モデルの登録数を確認
+
+        #テストデータ作成
+        sub = dsmodels.Subject(registrant=users.get_current_user(),
+                               subjectName='test_subject')
+        sub.put()
+        su = dsmodels.StudyUnit(registrant=users.get_current_user(),
+                                subject=sub,
+                                content='test_content',
+                                timeStamp='20130711',
+                                studyTimeHour=3,
+                                studyTimeMinute=30)
+        su.put()
+
+        studyUnits = dsmodels.StudyUnit.all().filter('registrant = ', users.get_current_user())
+        controler = mainControler()
+        studyTimes = controler.sumUpStudyHours(studyUnits)
+        self.assertEqual(studyUnits.count(), len(studyTimes), 'length not equals')
+
+        #テストデータ削除
+        sub.delete()
+        su.delete()
 
 if __name__ == '__main__':
     unittest.main()
